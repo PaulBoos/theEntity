10287f80cafc703c96829b99bfd8a3d25d25c4ac
GelberBecher <34918079+GelberBecher@users.noreply.github.com>
2021-06-05 19:21:09 +0200

    Initiation

---
+++ src/main/java/Facts.java
@@ -0,0 +1,87 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Facts {
+	
+	public static final String factURL = "facts.txt";
+	
+	public static String firstLine = "";
+	
+	public static int readInt(String fact) {
+		return Integer.parseInt(readString(fact));
+	}
+	
+	public static long readLong(String fact) {
+		return Long.parseLong(readString(fact));
+	}
+	
+	public static String readString(String fact) {
+		try {
+			DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("."), path -> path.toFile().isFile());
+			for(Path p: stream) {
+				if(p.toFile().getName().equals(factURL)) {
+					List<String> lines = Files.readAllLines(p);
+					for(String s: lines) {
+						if(s.startsWith(fact.replace(":","") + ":")) {
+							return s.substring(fact.length());
+						}
+					}
+					return "";
+				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+	public static void writeString(String fact, String value) {
+		try {
+			DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("."), path -> path.toFile().isFile());
+			for(Path path: stream) {
+				if(path.getFileName().toString().equals(factURL)) {
+					BufferedWriter writer = new BufferedWriter(new FileWriter(factURL, true));
+					List<String> lines = Files.readAllLines(path);
+					boolean inserted = false;
+					for(String s: lines) {
+						if(s.startsWith(fact + ":")) {
+							s = fact + ":" + value;
+							inserted = true;
+							break;
+						}
+						writer.append(s).append('\n');
+					}
+					if(!inserted) {
+						writer.append('\n').append(fact).append(':').append(value);
+					}
+					writer.close();
+				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static boolean readTokenFile() {
+		try {
+			DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("."), path -> path.toFile().isFile());
+			for(Path path: stream) {
+				if(path.getFileName().toString().equals("token")) {
+					List<String> lines = Files.readAllLines(path);
+					firstLine = lines.get(0);
+					return true;
+				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+}
---
+++ src/main/java/Tribe.java
@@ -0,0 +1,34 @@
+import net.dv8tion.jda.api.JDA;
+
+public enum Tribe {
+	
+	TEST("test", false, 848904154802159646L, 848904154802159646L),
+	VENGIR("Vengir", true, 0L, 836240032074825769L),
+	IMPERIUS("Imperius", true, 0L, 838865509658656839L),
+	BARDUR("Bardur", true, 0L, 826498545079025766L),
+	HOODRICK("Hoodrick", true, 0L, 826488999610023936L),
+	AIMO("Ai-Mo", true, 0L, 839923684427956315L);
+	
+	String name;
+	boolean visible;
+	long materialsChannel, actionChannel;
+	public static final Tribe[] visibleTribes = new Tribe[] {VENGIR, IMPERIUS, BARDUR, HOODRICK, AIMO};
+	public static final Tribe[] invisibleTribes = new Tribe[] {TEST};
+	public static final Tribe[] newTurnSubs = new Tribe[] {TEST, VENGIR, IMPERIUS, BARDUR, HOODRICK, AIMO};
+	
+	Tribe(String name, boolean visible, long materialsChannel, long actionChannel) {
+		this.name = name;
+		this.visible = visible;
+		this.materialsChannel = materialsChannel;
+		this.actionChannel = actionChannel;
+	}
+	
+	void announceNewTurn(JDA jda, int turn) {
+		try {
+			jda.getTextChannelById(actionChannel).sendMessage("```Turn #" + turn + " begins```").queue();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
