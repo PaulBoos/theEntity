2364c1238ad89e0f5ebe7e4f6fedd158c1a23336
GelberBecher <51286113+PaulBoos@users.noreply.github.com>
2021-06-27 17:29:09 +0200

    test Database moved to top Guild Database Accessor now outsourced from General Purpose Accessor

--- src/main/java/database/Accessor.java
+++ src/main/java/database/Accessor.java
@@ -1,9 +1,12 @@
 package database;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Accessor {
-
+	
+	
 	private final String DATABASEPATH;
 	protected Connection conn;
 	
@@ -20,43 +23,6 @@ public class Accessor {
 		if(conn == null) {
 			conn = DriverManager.getConnection(DATABASEPATH);
 		}
-	}
-	
-	public synchronized long getMember(long user) throws SQLException {
-		connect();
-		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE user = ?");
-		pstmt.setLong(0, user);
-		pstmt.execute();
-		ResultSet rs = pstmt.getResultSet();
-		return 0L;
-	}
-	
-	public synchronized long getGuild(long member) throws SQLException {
-		connect();
-		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE memberID = ?");
-		pstmt.setLong(0, member);
-		pstmt.execute();
-		ResultSet rs = pstmt.getResultSet();
-		try {
-			return rs.getLong("guildID");
-		} catch(SQLException throwables) {
-			throwables.printStackTrace();
-		}
-		return 0L;
-	}
-	
-	public synchronized long getUser(long member) throws SQLException {
-		connect();
-		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE memberID = ?");
-		pstmt.setLong(0, member);
-		pstmt.execute();
-		ResultSet rs = pstmt.getResultSet();
-		try {
-			return rs.getLong("userID");
-		} catch(SQLException throwables) {
-			throwables.printStackTrace();
-		}
-		return 0L;
 	}
 	
 }
---
+++ src/main/java/database/GuildDatabase.java
@@ -0,0 +1,54 @@
+package database;
+
+import java.sql.*;
+
+@SuppressWarnings("JpaQueryApiInspection")
+public class GuildDatabase extends Accessor {
+	
+	protected GuildDatabase(String path) {
+		super(path);
+	}
+	
+	public synchronized long getMember(long user) throws SQLException {
+		connect();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE user = ?");
+		pstmt.setLong(0, user);
+		pstmt.execute();
+		ResultSet rs = pstmt.getResultSet();
+		try {
+			return rs.getLong("memberID");
+		} catch(SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return 0L;
+	}
+	
+	public synchronized long getGuild(long member) throws SQLException {
+		connect();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE memberID = ?");
+		pstmt.setLong(0, member);
+		pstmt.execute();
+		ResultSet rs = pstmt.getResultSet();
+		try {
+			return rs.getLong("guildID");
+		} catch(SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return 0L;
+	}
+	
+	public synchronized long getUser(long member) throws SQLException {
+		connect();
+		PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM memberID WHERE memberID = ?");
+		pstmt.setLong(0, member);
+		pstmt.execute();
+		ResultSet rs = pstmt.getResultSet();
+		try {
+			return rs.getLong("userID");
+		} catch(SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return 0L;
+	}
+	
+}
